d717990661aa7d5c9a9d391cc5a031d399873678
Nabin sah <126480952+navin3456@users.noreply.github.com>
2024-11-10 21:12:29 +0545

    Update U.c

--- U.c
+++ U.c
@@ -14,7 +14,7 @@ void printdetails(struct teacherinfo t[10]){
   printf(".Name of the teacher=%s.\n.Teacher ID=%d.\n.Subject taught=%s.\n.Grade to teach=%d.\n",t->name,t->teacherid,t->subject,t->grade_to_teach);
 }
   int main(){
-    printf("Jharana Acharya\n");
+     
     int a=10,i;
     struct teacherinfo t[a];
     for(i=0;i<a;i++){
